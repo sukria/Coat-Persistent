b458ed0c42abcef78106165afd9d20f219ad7351
Alexis Sukrieh <sukria@sukria.net>
2009-06-18 06:57:58 +0000

    new types and POD for C::P:Types::MySQL

--- lib/Coat/Persistent/Types/MySQL.pm
+++ lib/Coat/Persistent/Types/MySQL.pm
@@ -41,7 +41,7 @@ coerce 'Int'
 # date
 
 subtype 'MySQL:Date'
-    => as 'Int'
+    => as 'Str'
     => where { /^\d{4}-\d\d-\d\d$/ };
 
 coerce 'MySQL:Date'
@@ -61,28 +61,66 @@ coerce 'Int'
         my ($year, $mon, $day) = /^(\d{4})-(\d\d)-(\d\d)$/;
         $year -= 1900;
         $mon--;
-        return mktime(0, 0, 0, ~~$day, $mon, $year);
-    };
-
-# year 
-
-subtype 'MySQL:Year'
-    => as 'Int'
-    => where { /^\d{4}$/ };
-
-coerce 'MySQL:Year'
-    => from 'Int'
-    => via { 
-        my ($sec, $min, $hour, $day, $mon, $year) = localtime($_);
-        return $year += 1900;
-    };
-
-coerce 'Int'
-    => from 'MySQL:Year'
-    => via {
-        my $year = $_;
-        $year -= 1900;
-        return mktime(0, 0, 0, 1, 0, $year);
+        return mktime(0, 0, 0, int($day), int($mon), $year);
     };
 
 1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Coat::Persistent::Types::MySQL -- Attribute types and coercions for MySQL data types
+
+=head1 DESCRIPTION
+
+The types defined in this module are here to provide simple and transparent
+storage of MySQL data types. This is done for atttributes you want to store 
+with a different value than the one the object has.
+
+For instance, if you have a datetime field, you may want to store it as a MySQL
+"datetime" format (YYYY-MM-DD HH:MM:SS) and handle it in your code as a
+timestamp, which is much more convinient for updates.
+
+This is possible by using the types defined in this module.
+
+=head1 EXAMPLE 
+
+We have a 'created_at' attribute, we want to handle it as a timestamp and store
+it as a MySQL datetime field.
+
+    use Coat::Persistent::Types::MySQL;
+
+    has_p 'created_at' => (
+        is => 'rw',
+        isa => 'Int', 
+        store_as => 'MySQL:DateTime, 
+    );
+
+Then, whenever a value that validates the MySQL:DateTime format is assigned to
+that field, it will be coerced to an Int. On the other hand, whenever an entry
+has to be saved, the value used for storage will be the result of a coercion
+from Int to MySQL:DateTime.
+
+=head1 TYPES
+
+The following types are provided by this module
+
+=over 4 
+
+=item MySQL:DateTime : YYYY-MM-DD HH:MM:SS
+
+=item MySQL:Date : YYYY-MM-DD
+
+=back
+
+=head1 SEE ALSO
+
+L<Coat::Types>, L<Coat::Persistent>
+
+=head1 AUTHOR
+
+Alexis Sukrieh <sukria@cpan.org>
+
+=cut
